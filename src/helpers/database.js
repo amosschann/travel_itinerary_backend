d2c64d863ebe45b645a121c70ba62403c93f20a6
Amos <amoschan1@gmail.com>
2023-09-17 11:23:57 +0800

    test new db connection method

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -1,18 +1,16 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// Database connection
-
-const database = mysql.createConnection({
+// Create a connection pool
+const pool = mysql.createPool({
   host: process.env.host,
   user: process.env.user,
   password: process.env.DATABASE_PASSWORD,
-  database: process.env.database
+  database: process.env.database,
+  waitForConnections: true,
+  connectionLimit: 10, 
+  queueLimit: 0,
 });
 
-database.connect((err => {
-  if (err) throw err;
-  console.log('MySQL Connected!');
-}));
 
-module.exports = database;
+module.exports = pool.promise();
